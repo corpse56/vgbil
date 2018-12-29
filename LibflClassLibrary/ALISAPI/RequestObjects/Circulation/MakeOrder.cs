2aa7e39431b76960b6c2c59a507e602b65b64a62
malekula <ka@libfl.ru>
2018-12-29 21:26:42 +0300

    потихоньку начал делать метод заказа книги.

---
+++ LibflClassLibrary/ALISAPI/RequestObjects/Circulation/MakeOrder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibflClassLibrary.ALISAPI.RequestObjects.Circulation
+{
+    class MakeOrder
+    {
+    }
+}
