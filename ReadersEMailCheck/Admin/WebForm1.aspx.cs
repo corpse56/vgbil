3633a7a147e079f75f76da4c564870b4a2d590fb
Roman Danilov <rdanilov75@gmail.com>
2019-05-08 16:23:49 +0300

    добавил шифрование пароля и кнопку выхода - 2

--- ReadersEMailCheck/Admin/WebForm1.aspx.cs
+++ ReadersEMailCheck/Admin/WebForm1.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -13,5 +14,12 @@ namespace ReadersEMailCheck
         {
             //string readersemail = Request.Form["Text1"];
         }
+        protected void SignOut_Click(object sender, EventArgs e)
+        {
+            FormsAuthentication.SignOut();
+            FormsAuthentication.RedirectToLoginPage();
+        }
     }
+
+
 }
